8d8b683190ad710d90306bc157efbc45db823a7d
Jacob Kostinek <kostinek.j@stu.janesville.k12.wi.us>
2023-04-24 13:44:00 -0500

    update

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -1,16 +1,20 @@
 function calc() {
-    var principle = Number(document.getElementById("principle").value);
+    var principal = Number(document.getElementById("principal").value);
     var rate = Number(document.getElementById("rate").value);
     var years = Number(document.getElementById("years").value);
     var monthly = Number(document.getElementById("monthly").value);
+    var N = 12;
     var j = (1 + rate/12)**(12*years);
-    output += principle * j - (monthly(j-1)) / (rate/12);
-    
-    var out = document.getElementById("output");
-    out.innerText = output;
+    var output = principal * j - (monthly(j-1)) / (rate/12);
+
+    document.getElementById("output").innerHTML = output;
+
 }
 
 function reset() {
-    var p = document.getElementById("output");
-    p.innerText = "";
+    document.getElementById("output").innerHTML = "";
+    document.getElementById("principal").value = "";
+    document.getElementById("rate").value = "";
+    document.getElementById("years").value = "";
+    document.getElementById("monthly").value = "";
 }
