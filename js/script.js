098683691dfc20c2dd7be6e09efab1a4537dac3c
Jacob Kostinek <kostinek.j@stu.janesville.k12.wi.us>
2023-05-30 14:05:27 -0500

    update

--- js/script.js
+++ js/script.js
@@ -20,18 +20,17 @@ showButton.addEventListener('click', () => {
   favDialog.showModal();
 });
 
-// "Favorite animal" input sets the value of the submit button
 selectEl.addEventListener('change', (e) => {
   confirmBtn.value = selectEl.value;
 });
 
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+
 favDialog.addEventListener('close', (e) => {
-  outputBox.value = favDialog.returnValue === 'default' ? "No return value." : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
+  outputBox.value = favDialog.returnValue === 'default' ? "No return value." : `ReturnValue: ${favDialog.returnValue}.`;
 });
 
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+
 confirmBtn.addEventListener('click', (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  favDialog.close(selectEl.value); // Have to send the select box value here.
+  event.preventDefault();
+  favDialog.close(selectEl.value);
 });
